0bc03688b2fc05bd3c60486485acdeeccc930e27
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-09-02 20:10:25 +0530

    No long read

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,8 @@ const Home = () => (
 						you’re on your way. Our custom made short-form videos cover the hottest and most crucial
 						news without any nerve-racking advertising.
         			</p>
-					<div className="mt-5 max-w-md mx-auto sm:block sm:justify-center md:mt-8">
-						<img className="w-full rounded-md shadow" src="/20200830_Cover@2x.png"></img>
+					<div className="mt-5 mx-auto sm:block sm:justify-center md:mt-8">
+						<img className="w-full md:w-4/5 mx-auto sfd-cover-image" src="/cover_img2.jpeg"></img>
 					</div>
 				</div>
 			</main>
@@ -34,9 +34,9 @@ const Home = () => (
       					</h3>
 					</div>
 
-					<div className="mt-20">
-						<ul className="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
-							<li>
+					<div className="mt-10 md:mt-20">
+						<ul className="flex flex-col-reverse md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+							<li className="mt-4">
 								<div className="flex">
 									<div className="mx-auto">
 										<img className="w-full md:w-3/4" src="/no_ads.svg"></img>
@@ -45,11 +45,33 @@ const Home = () => (
 							</li>
 							<li className="flex items-center">
 								<div className="flex">
-									<div className="ml-4">
+									<div className="ml-2 md:ml-4">
 										<h4 className="sfd-purple-text text-3xl leading-6 font-bold text-gray-900">No ads</h4>
 										<p className="mt-2 text-base leading-6 text-gray-800">
 											We believe that the best things in life are free. Free of advertisements that is. Our app delivers only news, no nerve-racking advertising or pop-ups.
               							</p>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+
+					<div className="mt-10">
+						<ul className="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+							<li className="flex">
+								<div className="flex">
+									<div className="ml-2 md:ml-14">
+										<h4 className="sfd-purple-text text-3xl leading-6 font-bold text-gray-900">No long reads</h4>
+										<p className="mt-2 text-base leading-6 text-gray-800">
+											Your daily dose of exitement through eight categories that cover the whole world – from scientific discoveries and world economy to arts and fashion news. Each story in 10 seconds. 24/7/365. Just swipe and enjoy!
+              							</p>
+									</div>
+								</div>
+							</li>
+							<li className="mt-4">
+								<div className="flex">
+									<div className="mx-auto flex md:justify-end">
+										<img className="w-full md:w-3/4" src="/barber.svg"></img>
 									</div>
 								</div>
 							</li>
