bb14bb96e9604df2f1305e08b6cf9ca54e72b361
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-09-03 20:45:32 +0530

    No fake news, download, feedback sections added

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home = () => (
 					</div>
 				</div>
 			</main>
-			<div className="py-12 bg-white mt-10">
+			<div className="pt-12 bg-white mt-10">
 				<div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-24">
 					<div className="lg:text-center">
 						<h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl sm:leading-10">
@@ -55,20 +55,23 @@ const Home = () => (
 							</li>
 						</ul>
 					</div>
-
+				</div>
+			</div>
+			<div className="sfd-no-long-read-section w-full py-12 md:py-0 bg-white md:h-68">
+				<div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-10 pt-1">
 					<div className="mt-10">
 						<ul className="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
-							<li className="flex">
+							<li className="flex pt-10">
 								<div className="flex">
-									<div className="ml-2 md:ml-14">
-										<h4 className="sfd-purple-text text-3xl leading-6 font-bold text-gray-900">No long reads</h4>
-										<p className="mt-2 text-base leading-6 text-gray-800">
+									<div className="ml-2 md:ml-28">
+										<h4 className="text-3xl leading-6 font-bold text-white">No long reads</h4>
+										<p className="mt-2 text-base leading-6 text-white">
 											Your daily dose of exitement through eight categories that cover the whole world – from scientific discoveries and world economy to arts and fashion news. Each story in 10 seconds. 24/7/365. Just swipe and enjoy!
               							</p>
 									</div>
 								</div>
 							</li>
-							<li className="mt-4">
+							<li className="mt-7">
 								<div className="flex">
 									<div className="mx-auto flex md:justify-end">
 										<img className="w-full md:w-3/4" src="/barber.svg"></img>
@@ -76,6 +79,119 @@ const Home = () => (
 								</div>
 							</li>
 						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="pt-2 bg-white">
+				<div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-24">
+					<div className="">
+						<ul className="flex flex-col-reverse md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+							<li className="mt-4">
+								<div className="flex">
+									<div className="mx-auto">
+										<img className="w-full md:w-3/4" src="/no_fake_news.svg"></img>
+									</div>
+								</div>
+							</li>
+							<li className="flex items-center">
+								<div className="flex">
+									<div className="ml-2 md:ml-4">
+										<h4 className="sfd-purple-text text-3xl leading-6 font-bold text-gray-900">No fake news</h4>
+										<p className="mt-2 text-base leading-6 text-gray-800">
+											Each one of our news stories is custom-made and created by a professional video editor and journalist. We do our own fact checking. No AI, no Google Translate, no funny business.
+              							</p>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="sfd-download-section py-12 bg-white mt-28">
+				<div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-24">
+					<div className="lg:text-center">
+						<h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl sm:leading-10">
+							Download the app and get swiping!
+      					</h3>
+					</div>
+
+					<div className="mt-10 md:mt-16">
+						<ul className="flex flex-col-reverse md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+							<li className="mt-4">
+								<div className="flex">
+									<div className="flex justify-end">
+										<img className="w-full md:w-8/12" src="/mobile.svg"></img>
+									</div>
+								</div>
+							</li>
+							<li className="flex">
+								<div className="flex">
+									<div className="mt-16">
+										<img className="w-36 pl-2" src="/Logo-With-Text.svg"></img>
+										<p className="mt-6 pl-2 pr-12 text-lg leading-6 text-gray-800">
+											Creative news templates and custom made videos with engaging audio give your ears and eyes a treat. We are never done improving and hope to offer you something exciting every time you open the So.Fa.Dog app.
+              							</p>
+										<div className="flex mt-28">
+											<img className="mr-1 w-2/5 cursor-pointer" src="/apple.svg"></img>
+											<img className="ml-1 w-2/5 cursor-pointer" src="/android.svg"></img>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="relative bg-white mt-20">
+				<div className="absolute inset-0">
+					<div className="absolute inset-y-0 left-0 w-1/2"></div>
+				</div>
+				<div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-6">
+					<div className="py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-8">
+						<div className="max-w-lg mx-auto">
+							<h2 className="sfd-purple-text text-2xl text-right leading-8 font-extrabold tracking-wide text-gray-900 sm:text-5xl sm:leading-10">
+								Got some feedbark?
+        					</h2>
+						</div>
+					</div>
+					<div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+						<div className="max-w-lg mx-auto lg:max-w-none">
+							<form action="#" method="POST" className="grid grid-cols-1 row-gap-6">
+								<div>
+									<label htmlFor="message" className="sr-only">Message</label>
+									<div className="relative rounded-md shadow-sm">
+										<textarea id="message" rows={4} className="sfd-form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div>
+									<label htmlFor="email" className="sr-only">Email</label>
+									<div className="relative rounded-md shadow-sm">
+										<input id="email" type="email" className="sfd-form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150" placeholder="Email" />
+									</div>
+								</div>
+								<div className="flex">
+									<div className="w-1/2 mr-4">
+										<label htmlFor="full_name" className="sr-only">Full name</label>
+										<div className="relative rounded-md shadow-sm">
+											<input id="full_name" className="sfd-form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150" placeholder="Full name" />
+										</div>
+									</div>
+									<div className="w-1/2 ml-4">
+										<label htmlFor="full_name" className="sr-only">Full name</label>
+										<div className="relative rounded-md shadow-sm">
+											<input id="full_name" className="sfd-form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150" placeholder="Full name" />
+										</div>
+									</div>
+								</div>
+								<div className="">
+									<span className="inline-flex rounded-md shadow-sm">
+										<button type="submit" className="sfd-feedback-btn inline-flex justify-center py-2 px-8 border border-transparent text-white text-base leading-6 font-medium rounded-md text-white focus:outline-none transition duration-150 ease-in-out">
+											SEND
+              							</button>
+									</span>
+								</div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,21 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            height: theme => ({
+                "screen/2": "50vh",
+                "screen/3": "calc(100vh / 3)",
+                "screen/4": "calc(100vh / 4)",
+                "screen/5": "calc(100vh / 5)",
+                "screen/1-3": "70vh",
+                "screen/1-4": "80vh",
+                "h-68": "18rem",
+                "h-72": "20rem",
+                "h-80": "24rem",
+                "h-82": "25rem",
+            }),
         },
     },
+    variants: { display: ["responsive", "hover", "focus"] },
     plugins: [
         require('@tailwindcss/ui'),
     ]
